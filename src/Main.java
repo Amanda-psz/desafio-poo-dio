1da67d55ccab51e582e967910d2bfb94cedc0410
Amanda-psz <prsouza.amanda@gmail.com>
2022-11-24 04:32:43 -0300

    inclusão de cursos e uma dev.

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,24 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descrição curso java");
-        curso1.setCargaHoraria(8);
+        curso1.setTitulo("curso lógica de programação");
+        curso1.setDescricao("descrição curso lógica de programação");
+        curso1.setCargaHoraria(5);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
         curso2.setDescricao("descrição curso js");
         curso2.setCargaHoraria(4);
+
+        Curso curso3 = new Curso();
+        curso3.setTitulo("curso java");
+        curso3.setDescricao("descrição curso java");
+        curso3.setCargaHoraria(8);
+
+        Curso curso4 = new Curso();
+        curso4.setTitulo("curso melhorando seu curriculo");
+        curso4.setDescricao("descrição curso melhorando seu curriculo");
+        curso4.setCargaHoraria(2);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria java");
@@ -33,6 +43,8 @@ public class Main {
         bootcamp.setDescricao("Descrição Bootcamp Java Developer");
         bootcamp.getConteudos().add(curso1);
         bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(curso3);
+        bootcamp.getConteudos().add(curso4);
         bootcamp.getConteudos().add(mentoria);
 
         Dev devMaria = new Dev();
@@ -59,6 +71,21 @@ public class Main {
         System.out.println("Conteúdos Inscritos João: " + devJoao.getConteudosInscritos());
         System.out.println("Conteúdos Concluídos João: " + devMaria.getConteudosConcluidos());
         System.out.println("XP: " + devJoao.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devAna = new Dev();
+        devAna.setNome("Ana");
+        devAna.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Ana: " + devAna.getConteudosInscritos());
+        devAna.progredir();
+        devAna.progredir();
+        devAna.progredir();
+        devAna.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Ana: " + devAna.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Ana: " + devAna.getConteudosConcluidos());
+        System.out.println("XP: " + devAna.calcularTotalXp());
     }
 
 
